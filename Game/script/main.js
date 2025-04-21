6cd48e4f9d13430aa7c54ce00856362603aed45e
Derek214 <depewdj@etsu.edu>
2025-04-21 11:02:44 -0400

    How to play now appears when start game is pressed

--- Game/script/main.js
+++ Game/script/main.js
@@ -5,3 +5,17 @@ window.onload = function () {
     const formattedDate = now.toLocaleDateString('en-US', options);
     document.getElementById("current-date").textContent = formattedDate;
 };
+
+
+//when game.html is first loaded how-to-play is displayed
+document.addEventListener('DOMContentLoaded', () => {
+    const hasSeenHelp = sessionStorage.getItem('hasSeenHelp');
+  
+    if (!hasSeenHelp) { // It will not display automatically again unless the page is closed
+      const helpModal = document.getElementById('helpModal');
+      if (helpModal) {
+        helpModal.style.display = 'block';
+        sessionStorage.setItem('hasSeenHelp', 'true');
+      }
+    }
+  });
